e472d048353442ee13a0c6909ce5b01cf29be5e6
Ansinho <ansin.4k@gmail.com>
2020-08-06 01:50:16 -0300

    2º day settings

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,9 @@
 import express from 'express'
 import routes from './routes'
-import cors from 'cors'
-const app = express();
+
+const app = express()
+
 app.use(express.json())
-app.use(cors())
 app.use(routes)
-app.listen(3333);
+
+app.listen(3333)
